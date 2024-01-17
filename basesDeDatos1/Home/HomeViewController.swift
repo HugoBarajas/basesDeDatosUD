b558192652b77034cc27b58901dca05815cc34b9
Angel Duarte <angel.duarte@stand8.io>
2024-01-16 19:24:50 -0600

    Fix

--- basesDeDatos1/Home/HomeViewController.swift
+++ basesDeDatos1/Home/HomeViewController.swift
@@ -8,100 +8,86 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-    
-    
-    let imageJanelly: UIImageView = {
-        let image = UIImageView()
-        image.image = UIImage(named: "janelly")
-        return image
-    }()
-    
-    
-    var viewModel : HomeViewModel!
   
+  var viewModel : HomeViewModel!
+  
+  let imageJanelly: UIImageView = {
+    let image = UIImageView()
+    image.image = UIImage(named: "janelly")
+    return image
+  }()
+  
+  var victorHugoPhoto: UIImageView = {
+    var image = UIImageView()
+    image.image = UIImage(named: "victorhugo")
     
-    func initJanelly() {
-        view.addSubview(imageJanelly)
-        imageJanelly.addAnchorsAndCenter(centerX: false, centerY: true, width: 150, height: 150, left: 15, top: nil, right: nil, bottom: nil)
-    }
-        
-    var victorHugoPhoto: UIImageView = {
-        var image = UIImageView()
-        image.image = UIImage(named: "victorhugo")
-        
-        return image
-    }()
-
+    return image
+  }()
+  
   var hugoPhoto : UIImageView = {
-      var image = UIImageView()
-      image.image = UIImage(named: "Hugo")
-      image.contentMode = .scaleAspectFill
-    
-      return image
-    }()
-
-    
-    var ana : UIImageView = {
-      var imageAna = UIImageView()
-      imageAna.image = UIImage(named: "Ana")
-      imageAna.contentMode = .scaleAspectFill
-        return imageAna
-    }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-      view.backgroundColor = .systemPink
-      view.backgroundColor = .systemPink
-   
-      initUI()
-    }
- 
+    var image = UIImageView()
+    image.image = UIImage(named: "Hugo")
+    image.contentMode = .scaleAspectFill
+    return image
+  }()
+  
+  var ana : UIImageView = {
+    var imageAna = UIImageView()
+    imageAna.image = UIImage(named: "Ana")
+    imageAna.contentMode = .scaleAspectFill
+    return imageAna
+  }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemPink
+    initUI()
+  }
+  
   func initUI(){
-      initAnaCard()
+    initAnaCard()
     initVicCard()
     initTekiCard()
-      initVictorHugo()
-        initHugoCard()
-         initJanelly()
+    initVictorHugo()
+    initHugoCard()
+    initJanelly()
   }
-
-  
   
   func initVicCard(){
-      var imageVic : UIImageView = {
-        var imageVic = UIImageView()
-        imageVic.image = UIImage(named: "VicImage")
-       
-        return imageVic
-      }()
+    var imageVic : UIImageView = {
+      var imageVic = UIImageView()
+      imageVic.image = UIImage(named: "VicImage")
       
-      view.addSubview(imageVic)
-      imageVic.addAnchorsAndSize(width: 150, height: 150, left: nil, top: nil, right: 15, bottom: 75)
+      return imageVic
+    }()
     
-    
-
+    view.addSubview(imageVic)
+    imageVic.addAnchorsAndSize(width: 150, height: 150, left: nil, top: nil, right: 15, bottom: 75)
   }
-
-    func initAnaCard(){
-        view.addSubview(ana)
-        ana.addAnchorsAndSize(width: 150, height: 150, left: 10, top: 65, right: nil, bottom: nil)
-    }
-    
+  
+  func initAnaCard(){
+    view.addSubview(ana)
+    ana.addAnchorsAndSize(width: 150, height: 150, left: 10, top: 65, right: nil, bottom: nil)
+  }
+  
+  func initJanelly() {
+    view.addSubview(imageJanelly)
+    imageJanelly.addAnchorsAndCenter(centerX: false, centerY: true, width: 150, height: 150, left: 15, top: nil, right: nil, bottom: nil)
+  }
+  
   func initTekiCard(){
     // Aqui van a crear su tarjrta de su foto
-
+    
   }
   
   func initHugoCard(){
     view.addSubview(hugoPhoto)
     hugoPhoto.addAnchorsAndCenter(centerX: false, centerY: true, width: 150, height: 150, left: nil, top: nil, right: 15, bottom: nil)
-    
   }
   
-  
-    func initVictorHugo(){
-        view.addSubview(victorHugoPhoto)
-        victorHugoPhoto.addAnchorsAndSize(width: 150, height: 150, left: 15, top: nil, right: nil, bottom: 75)
-    }
+  func initVictorHugo(){
+    view.addSubview(victorHugoPhoto)
+    victorHugoPhoto.addAnchorsAndSize(width: 150, height: 150, left: 15, top: nil, right: nil, bottom: 75)
+  }
 }
 
