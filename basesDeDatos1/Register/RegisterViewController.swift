1b9f3f4eb209f3467a8204cbd27d8dba19a6074d
Victor Hugo Barajas Santibañez <comodindelarge@gmail.com>
2024-01-17 21:41:46 -0600

    Password requirements, logic textField

--- basesDeDatos1/Register/RegisterViewController.swift
+++ basesDeDatos1/Register/RegisterViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 
 
-class RegisterViewController: UIViewController {
+class RegisterViewController: UIViewController, UITextFieldDelegate {
   var viewModel : RegisterViewModel!
 
   var names : UITextField = {
@@ -38,6 +38,7 @@ class RegisterViewController: UIViewController {
       var textField = UITextField()
       textField.placeholder = "Correo electrónico"
       textField.backgroundColor = .white
+      textField.keyboardType = .emailAddress
       return textField
     }()
     
@@ -84,13 +85,16 @@ class RegisterViewController: UIViewController {
     }()
     
 
+  var passwordRequeriments : UITextView = {
+    var textView = UITextView()
+    textView.backgroundColor = .clear
+    textView.textColor = .white
+    textView.font = UIFont(name: "Arial Bold", size: 15)
+    textView.text = "Debe contener minimo 8 caracteres, al menos una letra mayuscula y un numero, no podras utilizar una cotraseña anterior, asi como contraseñas faciles de adivinar (ejemplo: 123456)"
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-      view.backgroundColor = .red
-initUI()
-      
-    }
+    return textView
+  }()
+  
     
     var termisLabel : UILabel = {
             var termsLb = UILabel()
@@ -119,6 +123,14 @@ initUI()
         }()
 
     
+  override func viewDidLoad() {
+      super.viewDidLoad()
+    view.backgroundColor = .red
+initUI()
+    
+  }
+  
+  
   func initUI(){
     view.addSubview(names)
     names.addAnchorsAndCenter(centerX: true, centerY: false, width: width - 10, height: 50, left: nil, top: 90, right: nil, bottom: nil)
@@ -130,23 +142,31 @@ initUI()
       mothersMaidenName.addAnchorsAndCenter(centerX: true, centerY: false, width: width - 10, height: 50, left: nil, top: 10, right: nil, bottom: nil,withAnchor: .top, relativeToView: lastName)
     
       view.addSubview(phoneNumberTextField)
+    phoneNumberTextField.delegate = self
       phoneNumberTextField.addAnchorsAndCenter(centerX: true, centerY: false, width: width - 10, height: 50, left: nil, top: 10, right: nil, bottom: nil,withAnchor: .top, relativeToView: mothersMaidenName)
 
       view.addSubview(email)
+    email.delegate = self
       email.addAnchorsAndCenter(centerX: true, centerY: false, width: width - 10, height: 50, left: nil, top: 10, right: nil, bottom: nil,withAnchor: .top, relativeToView: phoneNumberTextField)
       
       view.addSubview(password)
+      password.delegate = self
       password.addAnchorsAndCenter(centerX: true, centerY: false, width: width - 10, height: 50, left: nil, top: 10, right: nil, bottom: nil,withAnchor: .top, relativeToView: email)
       
       view.addSubview(confirmPassword)
+      confirmPassword.delegate = self
       confirmPassword.addAnchorsAndCenter(centerX: true, centerY: false, width: width - 10, height: 50, left: nil, top: 10, right: nil, bottom: nil,withAnchor: .top, relativeToView: password)
+    
+    
+    view.addSubview(passwordRequeriments)
+    passwordRequeriments.addAnchorsAndSize(width: width - 10, height: 100, left: nil, top: 15, right: nil, bottom: nil, withAnchor: .top, relativeToView: confirmPassword)
       
    
-     /*  view.addSubview(termisLabel)
-            termisLabel.addAnchorsAndCenter(centerX: true, centerY: false, width: width - 10, height: 100, left: nil, top: 10, right: nil, bottom: nil,withAnchor: .top, relativeToView: promotions)
+    view.addSubview(termisLabel)
+            termisLabel.addAnchorsAndCenter(centerX: true, centerY: false, width: width - 10, height: 100, left: nil, top: 10, right: nil, bottom: nil,withAnchor: .top, relativeToView: passwordRequeriments)
             
-            view.addSubview(createAccountButton)
-            createAccountButton.addAnchorsAndCenter(centerX: true, centerY: false, width: width - 10, height: 50, left: nil, top: 10, right: nil, bottom: nil,withAnchor: .top, relativeToView: termisLabel)*/
+      view.addSubview(createAccountButton)
+            createAccountButton.addAnchorsAndCenter(centerX: true, centerY: false, width: width - 10, height: 50, left: nil, top: 10, right: nil, bottom: nil,withAnchor: .top, relativeToView: termisLabel)
 
     
   }
@@ -156,6 +176,41 @@ initUI()
      showPasswordButton.isSelected = !showPasswordButton.isSelected
        }
     
+  func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    
+    let maxLength : Int
+    
+    if textField == password || textField == confirmPassword{
+      
+      maxLength = 6
+      let currentString: NSString = textField.text! as NSString
+              
+      let newString: NSString =  currentString.replacingCharacters(in: range, with: string) as NSString
+      
+      return newString.length <= maxLength
+    }
+    
+    if textField == phoneNumberTextField{
+      let allowedCharacters = CharacterSet.decimalDigits
+          
+      let characterSet = CharacterSet(charactersIn: string)
+      
+      return allowedCharacters.isSuperset(of: characterSet)
+      
+    }
+    
+    if textField == email{
+     
+      let emailReg = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+      let emailTest = NSPredicate(format:"SELF MATCHES %@", emailReg)
+      return emailTest.evaluate(with: string)
+      
+    }
+    
+   
+    
+    return true
+  }
     
 
 }
