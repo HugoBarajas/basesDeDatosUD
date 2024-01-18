eed8df723528ce8d5bd381455830c9e7360b941a
AnaOlvera123 <personworkadd893@gmail.com>
2024-01-17 20:26:16 -0600

    Agregué el label Terminos y condiciones y boton para crear cuenta

--- basesDeDatos1/Register/RegisterViewController.swift
+++ basesDeDatos1/Register/RegisterViewController.swift
@@ -48,6 +48,33 @@ initUI()
         return pNumberTF
     }()
     
+    var termisLabel : UILabel = {
+            var termsLb = UILabel()
+            let originalText = "Al registrarte aceptas los Términos y Condiciones y Aviso de Privacidad de Ingenio"
+            let atributosTexto = NSMutableAttributedString(string: originalText)
+            let terms = (originalText as NSString).range(of: "Términos y Condiciones")
+                atributosTexto.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.white, range: terms)
+            let estiloSubrayado = [NSAttributedString.Key.underlineStyle: NSUnderlineStyle.single.rawValue]
+            atributosTexto.addAttributes(estiloSubrayado, range: terms)
+            let advice = (originalText as NSString).range(of: "Aviso de Privacidad")
+                atributosTexto.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.white, range: advice)
+            atributosTexto.addAttributes(estiloSubrayado, range: advice)
+                termsLb.attributedText = atributosTexto
+            termsLb.numberOfLines = 0
+            termsLb.textAlignment = .center
+            return termsLb
+        }()
+        
+        var createAccountButton : UIButton = {
+            var accountBtn = UIButton()
+            accountBtn.setTitle("Crear Cuenta", for: .normal)
+            accountBtn.setTitleColor(.black, for: .normal)
+            accountBtn.backgroundColor = .white
+            accountBtn.layer.cornerRadius = 25
+            return accountBtn
+        }()
+
+    
   func initUI(){
     view.addSubview(names)
     names.addAnchorsAndCenter(centerX: true, centerY: false, width: width - 10, height: 50, left: nil, top: 90, right: nil, bottom: nil)
@@ -60,6 +87,12 @@ initUI()
     
       view.addSubview(phoneNumberTextField)
       phoneNumberTextField.addAnchorsAndCenter(centerX: true, centerY: false, width: width - 10, height: 50, left: nil, top: 10, right: nil, bottom: nil,withAnchor: .top, relativeToView: mothersMaidenName)
+      
+     /*  view.addSubview(termisLabel)
+            termisLabel.addAnchorsAndCenter(centerX: true, centerY: false, width: width - 10, height: 100, left: nil, top: 10, right: nil, bottom: nil,withAnchor: .top, relativeToView: promotions)
+            
+            view.addSubview(createAccountButton)
+            createAccountButton.addAnchorsAndCenter(centerX: true, centerY: false, width: width - 10, height: 50, left: nil, top: 10, right: nil, bottom: nil,withAnchor: .top, relativeToView: termisLabel)*/
     
   }
 
